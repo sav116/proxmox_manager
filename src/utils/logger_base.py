c51d6486f323c5bff4148d84729907d1e75d906d
Artem Solovev <art.poker@mail.ru>
2023-11-05 23:50:54 +0300

    SECUR: add masking password and token

--- src/utils/logger_base.py
+++ src/utils/logger_base.py
@@ -1,15 +1,22 @@
 from loguru import logger
 import sys
 from datetime import datetime
+import re
+from data.config import config
 
-logger.add(sys.stderr, format="{time} {level} {message}", filter="my_module", level="INFO")
+
+def masked_out(message: str):
+    pattern = re.compile(f"({config.password}|{config.bot_token})")
+    return pattern.sub("*****", message)
 
 def logger_decorator(func):
     """Logger decorator"""
     def wrapper(*func_args, **kwargs):
-        logger.info(f"Calling function: {func.__name__}, with args: {func_args[1:]}")
+        logger.info(masked_out(f"Call: {func.__module__} {func.__name__} | args: {func_args[1:]}"))
         start_time = datetime.now()
         result = func(*func_args, **kwargs)
-        logger.info(f"Сompleted function: {func.__name__}, duration: {datetime.now() - start_time}, returned type: {type(result)}")
+        logger.info(masked_out(f"Сompleted: {func.__module__} {func.__name__} | returned type {type(result)}, size {sys.getsizeof(result)} | duration: {datetime.now() - start_time}"))
         return result
     return wrapper
+
+logger.add(sys.stderr, filter="my_module", level="INFO")
